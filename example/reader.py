9ed6bee2c89d8bae9866ca9621636c4d4503f58c
Unknown <benevedeselias@gmail.com>
2017-11-14 15:55:27 -0800

    IP Changes
    Testing again

--- example/reader.py
+++ example/reader.py
@@ -1,8 +1,8 @@
 import socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(("10.76.11.100", 1001))
 
-sock = sock.bind(("", 1001))
-
-with open("output.csv") as out:
+with open("output.csv", "w+") as out:
 	while True:
 		message, address = sock.recvfrom(1024)
 		out.write(message.decode("utf-8"))
