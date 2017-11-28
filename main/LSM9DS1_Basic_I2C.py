b32817e20fbf3f655e7997de31964da3307a8198
Unknown <benevedeselias@gmail.com>
2017-11-28 11:14:18 -0800

    Base data points on timestamp

--- main/LSM9DS1_Basic_I2C.py
+++ main/LSM9DS1_Basic_I2C.py
@@ -18,21 +18,21 @@ class Data():
         self.ts = []
 
     def update(self, dx, dy, dz, ts):
-        self.dx += [dx]
+        self.dx.append(dx)
         self.dx = self.dx[-100:]
-        self.dy += [dy]
+        self.dy.append(dy)
         self.dy = self.dy[-100:]
-        self.dz += [dz]
+        self.dz.append(dz)
         self.dz = self.dz[-100:]
 
         self.ts += [ts]
         self.ts = self.ts[-100:]
 
-        self.x += [self.x[-1] + (self.dx[-1]*(self.x[-1]-self.x[-2])/1000)]
+        self.x += [self.x[-1] + (self.dx[-1]*(self.ts[-1]-self.ts[-2])/1000)]
         self.x = self.x[-100:]
-        self.y += [self.y[-1] + (self.dy[-1]*(self.y[-1]-self.y[-2])/1000)]
+        self.y += [self.y[-1] + (self.dy[-1]*(self.ts[-1]-self.ts[-2])/1000)]
         self.y = self.y[-100:]
-        self.z += [self.z[-1] + (self.dz[-1]*(self.z[-1]-self.z[-2])/1000)]
+        self.z += [self.z[-1] + (self.dz[-1]*(self.ts[-1]-self.ts[-2])/1000)]
         self.z = self.z[-100:]
 
         # print("timestamp: ",[self.ts[-1]])
@@ -63,7 +63,7 @@ with open("output.csv", "w+") as out:
 
             data.update(time.time()*1000, gx, gy, gz)
 
-            print("z: " + str(data.z))
+            print("z: " + str(data.z[-1]))
 
             data.send(sock, UDP_IP, UDP_PORT)
             out.write(str(time.time()*1000) + "," + str(gx) + "," + str(gy) + "," + str(gz))
