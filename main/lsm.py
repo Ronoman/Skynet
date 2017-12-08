d93395b23fd4a0ef8b6f3eaaa43b7594734eed5b
Unknown <benevedeselias@gmail.com>
2017-12-08 09:18:32 -0800

    Experimenting with Trapezoid Rule

--- main/lsm.py
+++ main/lsm.py
@@ -148,11 +148,11 @@ class Gyro():
             self.dz = self.dz[-10:]
             self.ts = self.ts[-10:]
 
-            self.x += [self.x[-1] + (self.dx[-1]*(self.ts[-1]-self.ts[-2])/1000)] #Do a simple riemann sum between the last point and this one
+            self.x += [self.x[-1] + (((self.dx[-1]+self.dx[-2])/2)*(self.ts[-1]-self.ts[-2])/1000)] #Do a simple riemann sum between the last point and this one
             self.x = self.x[-10:]
-            self.y += [self.y[-1] + (self.dy[-1]*(self.ts[-1]-self.ts[-2])/1000)]
+            self.y += [self.y[-1] + (((self.dy[-1]+self.dy[-2])/2)*(self.ts[-1]-self.ts[-2])/1000)]
             self.y = self.y[-10:]
-            self.z += [self.z[-1] + (self.dz[-1]*(self.ts[-1]-self.ts[-2])/1000)]
+            self.z += [self.z[-1] + (((self.dz[-1]+self.dz[-2])/2)*(self.ts[-1]-self.ts[-2])/1000)]
             self.z = self.z[-10:]
 
             #time.sleep(0.0001)
