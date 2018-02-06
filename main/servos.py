dc240a1ac6589960030ac39f3cba3ba2a328d061
Unknown <izzietorres@yahoo.com>
2018-02-06 10:32:08 -0800

    moves some servos w/ user input

---
+++ main/servos.py
@@ -0,0 +1,28 @@
+#Playing with servos
+from threading import Thread
+import socket
+import pigpio
+import time
+
+def degToMs(degrees):
+    ret = (degrees + 90.0)/(180.0) * (2000.0) + 500.0
+    if(ret > 2500):
+        ret = 2500
+    elif(ret < 500):
+        ret = 500
+    return ret
+
+SERVO_PORT1 = 14
+SERVO_PORT2 = 15
+SERVO_PORT3 = 17
+SERVO_PORT4 = 18
+
+pi = pigpio.pi()
+pi.set_mode(SERVO_PORT1, pigpio.OUTPUT)
+
+while True:
+    deg = raw_input("Deg: ")
+    pi.set_servo_pulsewidth(SERVO_PORT1, degToMs(deg))
+    pi.set_servo_pulsewidth(SERVO_PORT2, degToMs(deg))
+    pi.set_servo_pulsewidth(SERVO_PORT3, degToMs(deg))
+    pi.set_servo_pulsewidth(SERVO_PORT4, degToMs(deg))
