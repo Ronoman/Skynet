2147c0a137806622288c40d61bde5c25606e3dd4
Unknown <izzietorres@yahoo.com>
2018-03-08 10:40:14 -0800

    Re-added pids, fixed pid errors, added max turn to servos

--- main/flight_control.py
+++ main/flight_control.py
@@ -13,8 +13,21 @@ while True:
     y = gyro.gety() #pitch, increasing backward
     z = gyro.getz() #yaw, increasing right
     if x != 0:
-        #fixt that
+        if x > 30:
+            x = 30
+        if x > -30:
+            x = 30
+        rAileron.set(-x) #If x is positive, plane is turning right, so rAileron moves counterclockwise (down) to turn plane back left
+        lAileron.set(-x) #If servos are mounted mirroring each other, rAileron and lAileron should be set to same thing
     if y != 0:
-        #fix that
+        if x > 30:
+            x = 30
+        if x > -30:
+            x = 30
+        stabilator.set(-y) #If y is positive, then plane is tilting back, so stabilator moves down to tilt it back down
     if z != 0:
-        #fix that
+        if x > 30:
+            x = 30
+        if x > -30:
+            x = 30
+        rudder.set(-z) #If z is positive, then plane is turning right, so rudder moves left to tilt it back left
