a1700f1b3fcbd9c42d0277aaddfb2b0b7213419e
MrRoboman4321 <benevedeselias@gmail.com>
2018-03-21 20:22:21 +0000

    XBee comms working, look at main/xbee_test.py

--- main/xbee_recv.py
+++ main/xbee_recv.py
@@ -2,14 +2,14 @@ import serial
 import time
 from xbee import XBee
 
-serial_port = serial.Serial('/dev/ttyUSB0', 9600)
+serial_port = serial.Serial('/dev/ttyS0', 9600, timeout=0.001)
 
 def print_data(data):
     print data
 
 xbee = XBee(serial_port, callback=print_data)
 
-while true:
+while True:
     try:
         time.sleep(0.001)
     except KeyboardInterrupt:
