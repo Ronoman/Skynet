01bcdf4a8fa4b185dfd6c23e8fbbd2940b98b01b
Unknown <izzietorres@yahoo.com>
2018-04-24 10:31:00 -0700

    Fixed pid things bc I'm dumb

--- main/flight_control.py
+++ main/flight_control.py
@@ -3,14 +3,18 @@ from servo_class import servo
 import lsm
 import time
 import pigpio
+import pid
 
 pi = pigpio.pi()
 gyro = lsm.Gyro()
 
 rAileron = servo(18)
 lAileron = servo(23)
+rollPID = pid.PID(0.0, 0.5, 0.0, 0.0)
 stabilator = servo(14)
+stabilatorPID = pid.PID(10.0, 0.5, 0.0, 0.0) #setpoint is 10 degrees for angle of attack
 rudder = servo(15)
+rudderPID = pid.PID(0.0, 0.5, 0.0, 0.0)
 
 while True:
     x = gyro.getx() #roll, increasing right
@@ -21,17 +25,17 @@ while True:
             x = 30
         if x > -30:
             x = 30
-        rAileron.set(aileronPID.update(x)) #If x is positive, plane is turning right, so rAileron moves counterclockwise (down) to turn plane back left
-        lAileron.set(aileronPID.update(x)) #If servos are mounted mirroring each other, rAileron and lAileron should be set to same thing
+        rAileron.set(rollPID.update(x)) #If x is positive, plane is turning right, so rAileron moves counterclockwise (down) to turn plane back left
+        lAileron.set(rollPID.update(x)) #If servos are mounted mirroring each other, rAileron and lAileron should be set to same thing
     if y != 0:
         if y > 30:
             y = 30
         if y > -30:
             y = 30
-        stabilator.set(-y) #If y is positive, then plane is tilting back, so stabilator moves down to tilt it back down
+        stabilator.set(stabilatorPID.update(y)) #If y is positive, then plane is tilting back, so stabilator moves down to tilt it back down
     if z != 0:
         if z > 30:
             z = 30
         if z > -30:
             z = 30
-        rudder.set(-z) #If z is positive, then plane is turning right, so rudder moves left to tilt it back left
+        rudder.set(rudderPID.update(z)) #If z is positive, then plane is turning right, so rudder moves left to tilt it back left
