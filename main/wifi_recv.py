f30ea3d295b5e92372e0d7f357942fe387b320e7
Unknown <benevedeselias@gmail.com>
2018-04-26 15:27:19 -0700

    Uncommented motor code

--- main/wifi_recv.py
+++ main/wifi_recv.py
@@ -46,8 +46,8 @@ while True:
         if(val > 2000):
             val = 2000
 
-        print("pigs s 12 " + str(val))
-        #os.system("pigs s 12 " + str(translate(message, 0, 0.5, 1000, 2000)))
+        #print("pigs s 12 " + str(val))
+        os.system("pigs s 12 " + str(translate(message, 0, 0.5, 1000, 2000)))
 
     elif(message[0] == "r_thumb_x"):
         val = translate(float(message[1]), -0.5, 0.5, SERVO_MIN, SERVO_MAX)
