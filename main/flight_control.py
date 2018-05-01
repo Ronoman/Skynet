15d9debc4f4da934ad0a00fe25444b14ad04f064
Unknown <izzietorres@yahoo.com>
2018-05-01 10:50:11 -0700

    fixing gyro orientation

--- main/flight_control.py
+++ main/flight_control.py
@@ -20,19 +20,19 @@ while True:
     x = gyro.getx() #roll, increasing right
     y = gyro.gety() #pitch, increasing backward
     z = gyro.getz() #yaw, increasing right
-    if x != 0:
-        if x > 30:
-            x = 30
-        if x < -30:
-            x = 30
-        rAileron.set(rollPID.update(x)) #If x is positive, plane is turning right, so rAileron moves counterclockwise (down) to turn plane back left
-        lAileron.set(rollPID.update(x)) #If servos are mounted mirroring each other, rAileron and lAileron should be set to same thing
     if y != 0:
         if y > 30:
             y = 30
         if y < -30:
             y = 30
-        stabilator.set(stabilatorPID.update(y)) #If y is positive, then plane is tilting back, so stabilator moves down to tilt it back down
+        rAileron.set(rollPID.update(x)) #If y is positive, plane is turning left, so must turn plane back right
+        lAileron.set(rollPID.update(x)) #If servos are mounted mirroring each other, rAileron and lAileron should be set to same thing
+    if x != 0:
+        if x > 30:
+            x = 30
+        if x < -30:
+            x = 30
+        stabilator.set(stabilatorPID.update(y)) #If x is positive, then plane is tilting back, so stabilator moves to tilt it back down
     if z != 0:
         if z > 30:
             z = 30
