bf5fd3799381b35ff5cec8a009f00403d1938011
Unknown <izzietorres@yahoo.com>
2018-05-04 10:20:48 -0700

    wrong servo pins

--- main/circle.py
+++ main/circle.py
@@ -9,12 +9,12 @@ pi = pigpio.pi()
 gyro = lsm.Gyro()
 t0 = int(round(time.time() * 1000))
 
-rAileron = servo(18)
-lAileron = servo(23)
+rAileron = servo(17)
+lAileron = servo(27)
 rollPID = pid.PID(0.0, -0.5, 0.0, 0.0)
-stabilator = servo(14)
+stabilator = servo(24)
 stabilatorPID = pid.PID(10.0, 1.0, 0.0, 0.0) #setpoint is 10 degrees for angle of attack
-rudder = servo(15)
+rudder = servo(23)
 rudderPID = pid.PID(0.0, 0.5, 0.0, 0.0)
 
 while True:
